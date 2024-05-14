aead6ad26b949da2b660590c6ad6c41a89c6bb26
AnthonyLoche <anthonylocheifc@gmail.com>
2024-05-14 20:48:04 -0300

    Retirar retiradas dos init.py

--- fabricaNeeds/views/__init__.py
+++ fabricaNeeds/views/__init__.py
@@ -1,7 +1,6 @@
 from .total import TotalViewSet
 from .estoque import EstoqueViewSet
 from .demandas import DemandasViewSet
-from .retiradas import RetiradasViewSet
 from .retirarEstoque import RetirarEstoqueViewSet
 from .entradasEstoque import EntradasEstoqueViewSet
 from .contribuicoes import ContribuinteViewSet
