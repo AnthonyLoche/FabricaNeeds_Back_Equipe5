2b792b77e8958625a370506479f203c313185df7
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-07-04 14:06:52 -0300

    FIx

--- core/fabricaNeeds/models/retiradas.py
+++ core/fabricaNeeds/models/retiradas.py
@@ -1,8 +1,8 @@
 from django.db import models
 # from .contribuicoes import Contribuinte
-from core.usuario.models import Usuario
+# from core.usuario.models import Usuario
 class Retiradas(models.Model):
-    retirante = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    # retirante = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     retirada = models.IntegerField()
     data = models.DateField(auto_now_add=True)
 
