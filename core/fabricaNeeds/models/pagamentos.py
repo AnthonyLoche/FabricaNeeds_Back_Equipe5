8a5613c10033577fe9b0782d9ad33101c500eb4e
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-07-14 17:01:49 -0300

    feat: Adicionar campos de data e descrição ao modelo de Pagamentos

--- core/fabricaNeeds/models/pagamentos.py
+++ core/fabricaNeeds/models/pagamentos.py
@@ -8,10 +8,14 @@ class Pagamentos(models.Model):
     email = models.EmailField(null=False)
     cpf = BRCPFField(max_length=11, null=False)
     valor = models.DecimalField(max_digits=10, decimal_places=2, null=False)
-    status = models.BooleanField(null=False)
+    status = models.CharField(max_length=15, null=False,default="Pendente")
+    data_pagamento = models.DateField(auto_now_add=True)
+    data_aprovacao = models.DateField(null=True)
+    descricao = models.CharField(max_length=100, null=True)
+    pix_copiacola = models.TextField(null=True)
     
     def __str__(self):
-        return f"{self.cliente} - {self.valor} - {self.status} - {self.email} - {self.cpf}"
+        return f"{self.cliente} - {self.valor} - {self.status} - {self.email} - {self.cpf} - {self.data_pagamento} - {self.data_aprovacao} - {self.descricao} - {self.pix_copiacola}"
     
     class Meta:
         verbose_name = "Pagamento"
